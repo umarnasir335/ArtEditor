4a3234650a6479062afab6f30b1b2b594b742864
Umar Nasir <umarnasir335@gmail.com>
2021-09-29 20:57:09 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from 'react';
+
+import Grid from './components/Grid';
+import ColorPicker from './components/ColorPicker';
+import useStyles from './App.styles';
+
+const offCell = {
+  on: false,
+  color: '#000000',
+};
+const initialCells = Array.from({ length: 40 }, () => offCell);
 
 function App() {
+  const [cells, setCells] = useState(initialCells);
+  const [currentColor, setCurrentColor] = useState('#56BC58');
+  const classes = useStyles();
+  const colorSwatch = useMemo(
+    () => [
+      ...new Set(cells.filter((cell) => cell.on).map((cell) => cell.color)),
+    ],
+    [cells]
+  );
+  const chatString = useMemo(
+    () => cells.map((cell) => cell.color.slice(1)).join(','),
+    [cells]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <ColorPicker currentColor={currentColor} onSetColor={setCurrentColor} />
+      <div className={classes.colorSwatchContainer}>
+        {colorSwatch.map((color) => (
+          <div
+            key={color}
+            onClick={() => setCurrentColor(color)}
+            className={classes.colorSwatch}
+            style={{ background: color }}
+          />
+        ))}
+      </div>
+      <Grid cells={cells} setCells={setCells} currentColor={currentColor} />
+      <p className={classes.chatString}>
+        {/* eslint-disable-next-line */}
+        !rgb
+        {' '}
+        {chatString}
+      </p>
     </div>
   );
 }
